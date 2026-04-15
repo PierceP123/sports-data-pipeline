a8c6cc2728016a36f764c3424aae42522015e050
PierceP123 <purcellpierce@gmail.com>
2026-04-15 17:16:27 +1000

    Updated Data

--- etl/run_pipeline.py
+++ etl/run_pipeline.py
@@ -1,10 +1,10 @@
-from extract_api_to_json import extract_data
-from transform_json import transform_data
+from extract_api_to_json import extract_api_to_json
+from transform_json import transform_json
 from load_to_bigquery import load_to_bigquery
 
 def main():
-    raw_data = extract_data()
-    df = transform_data(raw_data)
+    raw_data = extract_api_to_json()
+    df = transform_json(raw_data)
     load_to_bigquery(df)
 
 if __name__ == "__main__":
